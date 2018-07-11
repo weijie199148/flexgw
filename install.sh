ccf22da9d43ebba27e7734188c703045dad7b05e
weijie199148 <398080008@qq.com>
2018-07-11 17:38:24 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 ####----sysctl----####
-sysctl -a | egrep "ipv4.*(accept|send)_redirects" | awk -F "=" '{print $1 "=0"}'
- > a.txt
+sysctl -a | egrep "ipv4.*(accept|send)_redirects" | awk -F "=" '{print $1 "=0"}' > a.txt
 
 cat a.txt >>/etc/sysctl.conf
 
-cat /etc/sysctl.conf | sed -i 's/net.ipv4.ip_forward = 0/net.ipv4.ip_forward = 1
-/' /etc/sysctl.conf
+cat /etc/sysctl.conf | sed -i 's/net.ipv4.ip_forward = 0/net.ipv4.ip_forward = 1/' /etc/sysctl.conf
 sysctl -p
 ####----rpm----####
 yum -y install strongswan openvpn zip curl wget
@@ -16,8 +14,7 @@ rpm -ivh flexgw-2.5.0-1.el6.x86_64.rpm
 cp -f /usr/local/flexgw/rc/strongswan.conf /etc/strongswan/strongswan.conf
 cp -f /usr/local/flexgw/rc/openvpn.conf /etc/openvpn/server.conf
 
-cat /etc/strongswan/strongswan.d/charon/dhcp.conf | sed  -i 's/load = yes/#load
-= yes/' /etc/strongswan/strongswan.d/charon/dhcp.conf
+cat /etc/strongswan/strongswan.d/charon/dhcp.conf | sed  -i 's/load = yes/#load= yes/' /etc/strongswan/strongswan.d/charon/dhcp.conf
 
 >/etc/strongswan/ipsec.secrets
 
